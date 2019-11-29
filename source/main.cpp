6b75d39bb067d49ba8898c9bd6f4334f3ffd6dd0
Лесцов Борис <b.lestsov@corp.mail.ru>
2019-11-30 00:01:33 +0300

    mpi?

--- source/main.cpp
+++ source/main.cpp
@@ -23,6 +23,7 @@ float tau = T / (K-1);
 
 
 int rank = 0, comm_size;
+MPI_Comm MPI_CART_COMM;
 std::vector<int> loc_shape(3);
 int Nx, Ny, Nz;
 int Sx, Sy, Sz;
@@ -80,6 +81,27 @@ void anal(NdArr& res, int t){
     }
 }
 
+void send_recv(int src, int dst, NdArr& send, NdArr& recv){
+    MPI_Status status;
+    if (src >= 0){
+        if (dst >= 0){
+            MPI_Sendrecv(send.arr, send.size(), MPI_FLOAT,
+                        dst, 0, recv.arr, recv.size(),
+                        MPI_FLOAT, src, 0,
+                        MPI_CART_COMM, &status);
+        } else {
+            MPI_Send(send.arr, send.size(), MPI_FLOAT, dst, 0, MPI_CART_COMM);
+        }
+
+    } else {
+        if (dst >= 0){
+            MPI_Recv(recv.arr, recv.size(), MPI_FLOAT, src, 0, MPI_CART_COMM, &status);
+        } else {
+        }
+    }
+}
+
+
 void laplacian(NdArr& a, NdArr& out){
 
     float hx2 = hx*hx;
@@ -100,29 +122,177 @@ void laplacian(NdArr& a, NdArr& out){
         }
     }
 
+
     // send, recv ...
 
+    std::vector<int> shape_x(2);
+    shape_x[0] = Ny;
+    shape_x[1] = Nz;
+    NdArr max_x_send0(shape_x), max_x_sendN(shape_x);
+    for (int j = 0; j < Ny; ++j){
+        for (int k = 0; k < Nz; ++k){
+            out(0   , j, k) = 0;
+            out(Nx-1, j, k) = 0;
+            max_x_send0(j, k) = a(0, j, k);
+            max_x_sendN(j, k) = a(Nx-1, j, k);
+        }
+    }
+    NdArr max_x_recv0(shape_x), max_x_recvN(shape_x);
 
-    // periodic z
+    std::vector<int> shape_y(2);
+    shape_y[0] = Nx;
+    shape_y[1] = Nz;
+    NdArr max_y_send0(shape_y), max_y_sendN(shape_y);
+    for (int i = 0; i < Nx; ++i){
+        for (int k = 0; k < Nz; ++k){
+            out(i, 0,    k) = 0;
+            out(i, Ny-1, k) = 0;
+            max_y_send0(i, k) = a(i, 0   , k);
+            max_y_sendN(i, k) = a(i, Ny-1, k);
+        }
+    }
+    NdArr max_y_recv0(shape_y), max_y_recvN(shape_y);
+
+    std::vector<int> shape_z(2);
+    shape_z[0] = Nx;
+    shape_z[1] = Ny;
+    NdArr max_z_send0(shape_z), max_z_sendN(shape_z);
+    for (int i = 0; i < Nx; ++i){
+        for (int j = 0; j < Ny; ++j){
+            out(i, j, 0   ) = 0;
+            out(i, j, Nz-1) = 0;
+            max_z_send0(i, j) = a(i, j,    0);
+            max_z_sendN(i, j) = a(i, j, Nz-1);
+        }
+    }
+    NdArr max_z_recv0(shape_z), max_z_recvN(shape_z);
+
+
+    int src = -1, dst = -1;
+
+    MPI_Cart_shift(MPI_CART_COMM, 0, -1, &src, &dst);
+    send_recv(src, dst, max_x_send0, max_x_recv0);
+    MPI_Cart_shift(MPI_CART_COMM, 0, 1, &src, &dst);
+    send_recv(src, dst, max_x_sendN, max_x_recvN);
+
+    MPI_Cart_shift(MPI_CART_COMM, 1, -1, &src, &dst);
+    send_recv(src, dst, max_y_send0, max_y_recv0);
+    MPI_Cart_shift(MPI_CART_COMM, 1, 1, &src, &dst);
+    send_recv(src, dst, max_y_sendN, max_y_recvN);
+
+    MPI_Cart_shift(MPI_CART_COMM, 2, -1, &src, &dst);
+    send_recv(src, dst, max_z_send0, max_z_recv0);
+    MPI_Cart_shift(MPI_CART_COMM, 2, 1, &src, &dst);
+    send_recv(src, dst, max_z_sendN, max_z_recvN);
+
+    if (rank == 0){
+        //cout here
+    }
+
+
+    for (int j = 1; j < Ny-1; ++j){
+        for (int k = 1; k < Nz-1; ++k){
+            int i;
+
+            i = 0;
+            out(i, j, k) += (max_x_recv0(j, k)  -  2*a(i, j, k)  +  a(i+1, j, k)) / hx2;
+            out(i, j, k) += (a(i, j-1, k)       -  2*a(i, j, k)  +  a(i, j+1, k)) / hy2;
+            out(i, j, k) += (a(i, j, k-1)       -  2*a(i, j, k)  +  a(i, j, k+1)) / hz2;
+
+            i = Nx-1;
+            out(i, j, k) += (a(i-1, j, k)       -  2*a(i, j, k)  +  max_x_recvN(j, k)) / hx2;
+            out(i, j, k) += (a(i, j-1, k)       -  2*a(i, j, k)  +  a(i, j+1, k)     ) / hy2;
+            out(i, j, k) += (a(i, j, k-1)       -  2*a(i, j, k)  +  a(i, j, k+1)     ) / hz2;
+        }
+    }
+    for (int i = 1; i < Nx-1; ++i){
+        for (int k = 1; k < Nz-1; ++k){
+            int j;
+
+            j = 0;
+            out(i, j, k) += (a(i-1, j, k)       -  2*a(i, j, k)  +  a(i+1, j, k)) / hx2;
+            out(i, j, k) += (max_y_recv0(i, k)  -  2*a(i, j, k)  +  a(i, j+1, k)) / hy2;
+            out(i, j, k) += (a(i, j, k-1)       -  2*a(i, j, k)  +  a(i, j, k+1)) / hz2;
+
+            j = Ny-1;
+            out(i, j, k) += (a(i-1, j, k)       -  2*a(i, j, k)  +  a(i+1, j, k)     ) / hx2;
+            out(i, j, k) += (a(i, j-1, k)       -  2*a(i, j, k)  +  max_y_recvN(i, k)) / hy2;
+            out(i, j, k) += (a(i, j, k-1)       -  2*a(i, j, k)  +  a(i, j, k+1)     ) / hz2;
+        }
+    }
     for (int i = 1; i < Nx-1; ++i){
         for (int j = 1; j < Ny-1; ++j){
-            out(i, j, Nz-1) =
-                (a(i-1, j, Nz-2) - 2*a(i, j, Nz-1) + a(i+1, j, 1)) / hx2 +
-                (a(i, j-1, Nz-2) - 2*a(i, j, Nz-1) + a(i, j+1, 1)) / hy2 +
-                (a(i, j,   Nz-2) - 2*a(i, j, Nz-1) + a(i, j,   1)) / hz2;
+            int k;
+
+            k = 0;
+            out(i, j, k) += (a(i-1, j, k)       -  2*a(i, j, k)  +  a(i+1, j, k)) / hx2;
+            out(i, j, k) += (a(i, j-1, k)       -  2*a(i, j, k)  +  a(i, j+1, k)) / hy2;
+            out(i, j, k) += (max_z_recv0(i, j)  -  2*a(i, j, k)  +  a(i, j, k+1)) / hz2;
+
+            k = Nz-1;
+            out(i, j, k) += (a(i-1, j, k)       -  2*a(i, j, k)  +  a(i+1, j, k)   ) / hx2;
+            out(i, j, k) += (a(i, j-1, k)       -  2*a(i, j, k)  +  a(i, j+1, k)   ) / hy2;
+            out(i, j, k) += (a(i, j, k-1)       -  2*a(i, j, k)  +  max_z_recvN(i, j)) / hz2;
         }
     }
 
-    for (int i = 0; i < Nx; ++i){
+
+    // periodic z
+    // for (int i = 1; i < Nx-1; ++i){
+    //     for (int j = 1; j < Ny-1; ++j){
+    //         out(i, j, Nz-1) =
+    //             (a(i-1, j, Nz-2) - 2*a(i, j, Nz-1) + a(i+1, j, 1)) / hx2 +
+    //             (a(i, j-1, Nz-2) - 2*a(i, j, Nz-1) + a(i, j+1, 1)) / hy2 +
+    //             (a(i, j,   Nz-2) - 2*a(i, j, Nz-1) + a(i, j,   1)) / hz2;
+    //     }
+    // }
+
+    if (Px == 0) {
         for (int j = 0; j < Ny; ++j){
-            out(0   , i,    j) = 0;
-            out(Nx-1, i,    j) = 0;
-            out(i   , 0   , j) = 0;
-            out(i   , Ny-1, j) = 0;
+            for (int k = 0; k < Nz; ++k){
+                out(0   , j,    k) = 0;
+            }
+        }
+    }
+    if (Px == NPx-1) {
+        for (int j = 0; j < Ny; ++j) {
+            for (int k = 0; k < Nz; ++k){
+                out(Nx-1, j,    k) = 0;
+            }
+        }
+    }
+
+
+
+    if (Py == 0) {
+        for (int i = 0; i < Nx; ++i){
+            for (int k = 0; k < Nz; ++k){
+                out(i   , 0   , k) = 0;
+            }
+        }
+    }
+    if (Py == NPy-1) {
+        for (int i = 0; i < Nx; ++i){
+            for (int k = 0; k < Nz; ++k){
+                out(i   , Ny-1, k) = 0;
+            }
+        }
+    }
+
 
             // type 1
-            // out(i, j, 0  ) = 0;
-            // out(i, j, N-1) = 0;
+    if (Pz == 0) {
+        for (int i = 0; i < Nx; ++i){
+            for (int j = 0; j < Ny; ++j){
+                out(i, j, 0  ) = 0;
+            }
+        }
+    }
+    if (Pz == NPz-1) {
+        for (int i = 0; i < Nx; ++i){
+            for (int j = 0; j < Ny; ++j){
+                out(i, j, Nz-1) = 0;
+            }
         }
     }
 
@@ -149,6 +319,18 @@ int main(int argc, char** argv){
         NPx = n_side;
         NPy = n_side;
         NPz = n_side;
+        int dims[] = {NPx, NPy, NPz};
+        int coords[] = {-1, -1, -1};
+
+
+        // TODO: FIX 0, 0, 1
+        int periods[] = {0, 0, 0};
+
+        MPI_Cart_create(MPI_COMM_WORLD, 3, dims, periods, true, &MPI_CART_COMM);
+        MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        MPI_Cart_coords(MPI_CART_COMM, rank, 3, coords);
+
 
         Nx = N/NPx;
         Ny = N/NPy;
@@ -158,9 +340,12 @@ int main(int argc, char** argv){
         loc_shape[2] = Nz;
 
 
-        Px = rank/NPy/NPz;
-        Py = (rank - Px*NPy*NPz) / NPy;
-        Pz = (rank - Px*NPy*NPz - Py*NPy);
+        // Px = rank/NPy/NPz;
+        // Py = (rank - Px*NPy*NPz) / NPy;
+        // Pz = (rank - Px*NPy*NPz - Py*NPy);
+        Px = coords[0];
+        Py = coords[1];
+        Pz = coords[2];
 
         Sx = Px*Nx;
         Sy = Py*Ny;
